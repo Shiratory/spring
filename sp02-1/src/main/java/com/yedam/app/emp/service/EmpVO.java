ea3f345cf89b9ed829f2f4cf7950e1579f878d04
Shiratory <do880617@gmail.com>
2025-07-01 17:49:48 +0900

    20250701 spring 조회/추가

---
+++ sp02-1/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -0,0 +1,22 @@
+package com.yedam.app.emp.service;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor // @Builder시 생략되므로 명시적으로 추가함
+public class EmpVO {
+	// employee_id => employeeId => setEmployeeId
+	private Integer employeeId;
+	private String lastName;
+	private String email;
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+}
--- sp02-1/src/test/java/com/yedam/app/Sp021ApplicationTests.java
+++ sp02-1/src/test/java/com/yedam/app/Sp021ApplicationTests.java
@@ -1,13 +1,71 @@
 package com.yedam.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
 
 @SpringBootTest
 class Sp021ApplicationTests {
+	
+	@Autowired // 필드주입 : 테스트용
+	private EmpMapper empMapper;
+	
+	// @Test // 해당 메소드를 테스트하겠다고 선언
+	public void selectAll() {
+		List<EmpVO> list = empMapper.selectAll();
+		for(EmpVO emp : list) {
+			System.out.println(emp.getLastName());
+		}
+		
+		assertTrue(!list.isEmpty());
+	}
+	
+	//@Test
+	public void selectOne() {
+		EmpVO emp = EmpVO.builder()
+						.employeeId(100)
+						.build();
+		
+		EmpVO findVO = empMapper.selectInfo(emp);
+		System.out.println(findVO);
+		
+		assertEquals("King", findVO.getLastName());
+	}
+	
+	// @Test
+//	public void insertValue() {
+//		EmpVO emp = EmpVO.builder()
+//							.employeeId(1001)
+//							.lastName("Kang")
+//							.email("kg1@google.com")
+//							.jobId("SA_REP")
+//							.build();
+//		
+//		int result = empMapper.insertInfo(emp);
+//		
+//		assertEquals(1, result);
+//	}
 
 	@Test
-	void contextLoads() {
+	public void insertSelectKey() {
+		EmpVO emp = EmpVO.builder()
+					     .lastName("Hong")
+					     .email("hong@naver.com")
+					     .jobId("IT_PROG")
+					     .salary(1200)
+					     .build();
+		
+		int result = empMapper.insertInfo(emp);
+		System.out.println("사원번호 : " + emp.getEmployeeId());
+		
+		assertEquals(1, result);
 	}
-
 }
