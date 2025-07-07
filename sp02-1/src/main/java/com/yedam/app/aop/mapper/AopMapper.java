27c82dce955dd05b6013dc403e37da1effb59cf7
Shiratory <do880617@gmail.com>
2025-07-07 15:56:59 +0900

    20250707-1

--- sp02-1/src/main/java/com/yedam/app/aop/mapper/AopMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yedam.app.aop.mapper;
-
-import org.apache.ibatis.annotations.Insert;
-
-public interface AopMapper {	
-	@Insert("INSERT INTO t_aop_test VALUES(#{value})")
-	public int insert(String value);
-}
--- sp02-1/src/main/java/com/yedam/app/aop/service/AopService.java
+++
@@ -1,5 +0,0 @@
-package com.yedam.app.aop.service;
-
-public interface AopService {
-	public void insert();
-}
